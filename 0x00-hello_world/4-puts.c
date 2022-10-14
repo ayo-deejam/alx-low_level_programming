a9fa903862f421cc53a0a0e1e56059c4daf84cd9
ayo-deejam <abdulmajeedsalami@gmail.com>
2022-10-14 03:05:43 -0700

    Put string to stdio

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,13 +3,13 @@
 /**
  * * main - Entry point
  *
- * * Description: 'this programme prints to the standard utput using puts function'
+ * * Description: 'prints to the standard output using puts function'
  *
  * * Return: Always 0 (Success)
  */
 int main(void)
 {
-char string[] = "\"Programming is like building a multilingual puzzle\n";
+char string[] = "\"Programming is like building a multilingual puzzle";
 puts(string);
 return (0);
 }
