bc82fb8caf4a58e951285b26a88607aa4d6e02bf
ayo-deejam <abdulmajeedsalami@gmail.com>
2022-10-14 02:16:07 -0700

    Print using Puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 int main(void) {
-char string[60];
-string = "Programming is like building a multilingual puzzle";
+char string[] = "Programming is like building a multilingual puzzle";
 puts(string);
 return (0);
 }
