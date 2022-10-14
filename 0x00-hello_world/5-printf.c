c122956ce0f308c1aa054eab74ae434b02c726f1
ayo-deejam <abdulmajeedsalami@gmail.com>
2022-10-14 03:25:05 -0700

    Print string to stdio

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * * main - Entry point
+ *
+ * * Description: 'prints to the standard output using printf function'
+ *
+ * * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char string[] = "with proper grammar, but the outcome is a piece of art,";
+printf("%c\n", string);
+return (0);
+}
