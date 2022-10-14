a6f98529d4f0e26cec8812e76183c9f94c574a82
ayo-deejam <abdulmajeedsalami@gmail.com>
2022-10-14 14:32:24 -0700

    exclusion

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -15,7 +15,11 @@ int main(void)
 char alpha = 'a';
 while (alpha <= 'z')
 {
-if (alpha != 'e' && alpha != 'q')
+if (alpha == 'e' || alpha == 'q')
+{
+alpha++
+}
+else
 {putchar(alpha);
 alpha++;
 }
