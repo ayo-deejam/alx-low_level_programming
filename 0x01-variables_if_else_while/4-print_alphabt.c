0d6a02c93e6267d0b8142402e79d25dc70c54fb7
ayo-deejam <abdulmajeedsalami@gmail.com>
2022-10-14 14:15:35 -0700

    exclusion

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'prints excludes e and q'
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+char alpha = 'a';
+while (alpha <= 'z')
+{
+if(alpha != 'e' || alpha != 'q')
+{putchar(alpha);
+alpha++;
+}
+}
+putchar('\n');
+return (0);
+}
