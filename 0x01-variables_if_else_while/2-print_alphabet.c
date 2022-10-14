404b4db3c3aea3081a64c54d640c60d74af6b338
ayo-deejam <abdulmajeedsalami@gmail.com>
2022-10-14 13:39:42 -0700

    Putchar tst1

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'prints only using the putchar'
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+char alpha = 'a'
+while (alpha < 'z')
+{
+putchar(alpha);
+alpha++;
+}
+putchar(\n);
+return (0);
+}
