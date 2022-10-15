f953145146222badb5fe6f0306f2e0a346a09201
ayo-deejam <abdulmajeedsalami@gmail.com>
2022-10-15 03:01:45 -0700

    Advanced task 1

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'prints only using the putchar'
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+int n = 0;
+int m;
+while (n < 100)
+	{
+	if (n < 10)
+		{
+		putchar('0');
+		putchar(n + '0');
+		}
+	else
+		{
+		for (m = 99, m < 10, m--)
+			{
+			if (n / 10 + n % 10 == m / 10 + m % 10  || n / 10 == n % 10)
+				{
+				n++;
+				}
+			else
+				{
+				putchar(n + '0');
+				}
+			}
+		}
+	putchar(',');
+	putchar(' ');
+	}
+	n++;
+}
+putchar('\n');
+return (0);
+}
