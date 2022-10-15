08b2e01fc4ce5bcb36602931da9488ca9611b297
ayo-deejam <abdulmajeedsalami@gmail.com>
2022-10-15 09:21:58 -0700

    combo3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,19 +12,19 @@
  */
 int main(void)
 {
-int n = 0;
+int n = 1;
 int m;
 int col;
 int row = 0;
 
-while (n < 99)
+while (n < 91)
 {
 for (col = 0; col < 100; col += 10)
 {
 	row++;
 	for (m = 1; m < 10; m++)
 	{
-		if (n < col + row)
+		if (n < (col + row))
 		{
 			n++;
 		}
@@ -34,8 +34,8 @@ for (col = 0; col < 100; col += 10)
 			putchar(n % 10 + '0');
 			putchar(',');
 			putchar(' ');
+			n++;
 		}
-		n++;
 	}
 }
 }
