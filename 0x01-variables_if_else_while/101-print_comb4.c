6276afcf5a71fdce1a57a85324ab25bdaac06330
ayo-deejam <abdulmajeedsalami@gmail.com>
2022-10-16 10:01:57 -0700

    Combo of 3 digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'prints only using the putchar'
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+int n = 1;
+int outer_block;
+int counter;
+int block_counter = -1;
+
+for (outer_block = 0; outer_block < 10; outer_block ++)
+{
+	block_counter++
+		for (counter = 0; counter < 100; counter++)
+		{
+			if (n / 10 <= 11 * block_counter || n / 10 % 10 <= n % 10  || n % 10 == 0)
+		{
+		}
+		else
+		{
+			putchar(n / 100 + '0');
+			putchar(n / 10 % 10 + '0');
+			putchar(n % 10 + '0');
+			if (n < 789)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		n++;
+		}
+}
+putchar('\n');
+return (0);
+}
