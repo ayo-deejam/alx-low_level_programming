5a3c3a8331c1c7beca4a25919142b01ec04eda7d
ayo-deejam <abdulmajeedsalami@gmail.com>
2022-11-15 20:45:06 -0800

    Create header file dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - creates a struct of a dog
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Name of dog owner
+ *
+ * Description: This is part of ALX C Programming course
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
