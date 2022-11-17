79ae6c00fe172f4613a2548c4e688439e9871ee5
ayo-deejam <abdulmajeedsalami@gmail.com>
2022-11-17 14:45:07 -0800

    Done

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
 #include "function_pointers.h"
-
 /**
  * print_name - prints a name.
  * @name: input name.
@@ -10,5 +9,5 @@
 void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
-	f(name);
+		f(name);
 }
