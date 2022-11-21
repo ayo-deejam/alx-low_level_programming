b85166ff2409afbbb5036579a01a3fdec86bda9a
ayo-deejam <abdulmajeedsalami@gmail.com>
2022-11-21 07:20:05 -0800

    Print putchar 2

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,12 +8,12 @@ int main(void)
 {
 	char string[] = "_putchar";
 	int i = 0;
-
 	int len_of_array = sizeof(string) / sizeof(string[0]);
-	for (i = 0; i < len_of_array; i++)
+
+	for (i = 0; i < len_of_array - 1; i++)
 	{
 		_putchar(string[i]);
 	}
 	_putchar('\n');
-return(0);
+return (0);
 }
