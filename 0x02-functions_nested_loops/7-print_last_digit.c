6b89b7190e8586f09f52e0a8912a09a15e32f628
ayo-deejam <abdulmajeedsalami@gmail.com>
2022-11-21 11:53:41 -0800

    Print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,7 +10,19 @@ int print_last_digit(int num)
 {
 	int last_digit;
 
-	last_digit = num % 10;
-	_putchar('0'+last_digit);
-	return (last_digit);
+	int print(int num)
+	{
+		last_digit = num % 10;
+		_putchar('0' + last_digit);
+		return (last_digit);
+	}
+	if (num < 0)
+	{
+		num = num * (-1);
+		print(num)
+	}
+	else
+	{
+		print(num)
+	}
 }
