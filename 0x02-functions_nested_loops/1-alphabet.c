0a787009ed986fb7c2991edaf90268ddd8212d75
ayo-deejam <abdulmajeedsalami@gmail.com>
2022-11-21 07:35:13 -0800

    Print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - Prints aalphabets in lowercase
+ *
+ * Return: No return value
+ */
+void print_alphabet(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		_putchar(alphabet);
+		alphabet++
+	}
+	_putchar('\n');
+}
