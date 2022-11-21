0908dfdca984c5920c3ab7115e19b0ae15871c6a
ayo-deejam <abdulmajeedsalami@gmail.com>
2022-11-21 12:02:27 -0800

    Printing last digit with abs

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,26 +3,15 @@
  * print_last_digit - Prints the last digit
  * of a number
  * @num: Input number
+ * @last_digit: Last digit of input number
  *
  * Return: Last digit of the number
  */
 int print_last_digit(int num)
 {
-	int last_digit;
+	num = _abs(num);
+	int last_digit = num % 10;
 
-	int print(int num)
-	{
-		last_digit = num % 10;
-		_putchar('0' + last_digit);
-		return (last_digit);
-	}
-	if (num < 0)
-	{
-		num = num * (-1);
-		print(num)
-	}
-	else
-	{
-		print(num)
-	}
+	_putchar('0' + last_digit);
+	return (last_digit);
 }
