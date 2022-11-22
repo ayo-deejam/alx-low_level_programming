b266b2c6703de7a60b0d0ec23d7260c586dda179
ayo-deejam <abdulmajeedsalami@gmail.com>
2022-11-22 02:03:34 -0800

    Updated function

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,11 +2,24 @@
 /**
  * print_last_digit - Prints the last digit
  * of a number
+ * _abs - Computes absolute value of a number
  * @num: Input number
  * @last_digit: Last digit of input number
  *
  * Return: Last digit of the number
  */
+int _abs(int num)
+{
+	if (num < 0)
+        {
+                return (num * (-1));
+        }
+        else
+        {
+                return (num);
+        }
+}
+
 int print_last_digit(int num)
 {
 	int last_digit;
