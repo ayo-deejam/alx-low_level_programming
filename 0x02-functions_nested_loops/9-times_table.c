eb710a2fd78ce11daecdfc850dc122646d342df9
ayo-deejam <abdulmajeedsalami@gmail.com>
2022-11-22 04:18:18 -0800

    9 times

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * times_table - 9 multiplication table
+ *
+ * Return: returns nothing
+ */
+void times_table()
+{
+	int count_1;
+	int count_2 = 0;
+
+	while (count_2 < 10)
+	{
+		count_1 = 0;
+		while (count_1 < 10)
+		{
+			if (count_1 == 0)
+			{
+				_putchar('0');
+				_putchar(',');
+				count1++;
+			}
+			else
+			{
+				num = count_1 * count_2;
+				if (num < 10)
+				{
+					_putchar(32);
+					_putchar(32);
+					_putchar('0' + num);
+					_putchar(',');
+					count_1++
+				}
+				else
+				{
+					_putchar(32);
+					_putchar('0' + (num / 10));
+					_putchar('0' + (num % 10));
+					_putchar(',');
+					count_1++;
+				}
+			}
+		}
+		count_2++;
+	}
+}
