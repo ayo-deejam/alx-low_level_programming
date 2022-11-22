8ebb96252905a726741b039c5d640ca37bbfdae8
ayo-deejam <abdulmajeedsalami@gmail.com>
2022-11-22 04:49:36 -0800

    9 times - Correct format

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,9 +6,7 @@
  */
 void times_table(void)
 {
-	int num;
-	int count_1;
-	int count_2 = 0;
+	int num, count_1, count_2 = 0;
 
 	while (count_2 < 10)
 	{
@@ -18,8 +16,6 @@ void times_table(void)
 			if (count_1 == 0)
 			{
 				_putchar('0');
-				_putchar(',');
-				count_1++;
 			}
 			else
 			{
@@ -29,23 +25,19 @@ void times_table(void)
 					_putchar(32);
 					_putchar(32);
 					_putchar('0' + num);
-					/*_putchar(',');
-					count_1++;*/
 				}
 				else
 				{
 					_putchar(32);
 					_putchar('0' + (num / 10));
 					_putchar('0' + (num % 10));
-					/*_putchar(',');
-					count_1++;*/
 				}
-				if (count_1 <= 8)
+			}
+			if (count_1 <= 8)
 				{
 					_putchar(',');
 				}
-				count_1++;
-			}
+			count_1++;
 		}
 		_putchar('\n');
 		count_2++;
