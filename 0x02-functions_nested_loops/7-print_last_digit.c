1cb045defc4642608f4f742a5da70b51fe1b4824
ayo-deejam <abdulmajeedsalami@gmail.com>
2022-11-22 02:51:35 -0800

    Fix calculate min interger

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,30 +3,25 @@
 /**
  * print_last_digit - Prints the last digit
  * of a number
- * _abs - Computes absolute value of a number
  * @num: Input number
  * @last_digit: Last digit of input number
  *
  * Return: Last digit of the number
  */
-int _abs(int num)
-{
-	if ((long long int) num < 0)
-        {
-                return (num * (-1));
-        }
-        else
-        {
-                return (num);
-        }
-}
 
 int print_last_digit(int num)
 {
 	int last_digit;
 
-	(long long int) num = _abs(num);
-   	last_digit = num % 10;
-	_putchar('0' + last_digit);
+	if (num < 0)
+	{
+		last_digit = (num % 10) * (-1);
+		
+	}
+	else
+	{
+		last_digit = num % 10;
+	}
+	_putchar("0" + last_digit);
 	return (last_digit);
 }
