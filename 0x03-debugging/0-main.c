c25c30f2c2b80739b0fa49c45e70d651b93b6642
ayo-deejam <abdulmajeedsalami@gmail.com>
2022-11-23 11:32:36 -0800

    Test postive negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  * main - tests the positive or negative function
+  * the test case is for only case 0
+  *
+  * Return: 0
+  */
+
+int main(void)
+{
+	positive_or_negative(0);
+
+	return (0);
+
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+This is to solve the debugging tax on ALX course
