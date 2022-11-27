d119a661fd7daa7c5151291fcece9ee458ad700f
ayo-deejam <abdulmajeedsalami@gmail.com>
2022-11-27 15:21:31 -0800

    Changed putchar

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -26,11 +26,11 @@ void _print(char *str, int l)
 		if (str[i] != '0')
 			j = 1;
 		if (j || i == l - 1)
-			_putchar(str[i]);
+			putchar(str[i]);
 		i++;
 	}
 
-	_putchar('\n');
+	putchar('\n');
 	free(str);
 }
 
@@ -126,7 +126,7 @@ int main(int argc, char *argv[])
 	if (argc != 3 || check_for_digits(argv))
 	{
 		for (ti = 0; e[ti]; ti++)
-			_putchar(e[ti]);
+			putchar(e[ti]);
 		exit(98);
 	}
 	for (l1 = 0; argv[1][l1]; l1++)
@@ -138,7 +138,7 @@ int main(int argc, char *argv[])
 	if (a == NULL)
 	{
 		for (ti = 0; e[ti]; ti++)
-			_putchar(e[ti]);
+			putchar(e[ti]);
 		exit(98);
 	}
 	init(a, ln - 1);
@@ -148,7 +148,7 @@ int main(int argc, char *argv[])
 		if (t == NULL)
 		{
 			for (ti = 0; e[ti]; ti++)
-				_putchar(e[ti]);
+				putchar(e[ti]);
 			free(a);
 			exit(98);
 		}
